8f59b331f000d27119260d22be909cfc50fd42b6
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2021-08-08 16:59:43 +0500

    cleanup startup script a little

--- .config/i3/startup.fish
+++ .config/i3/startup.fish
@@ -14,33 +14,6 @@ function launch
             workspace $start_workspace"
 end
 
-# sleep 5
-
 launch "2:chrome" "chrome" "google-chrome-stable"
 launch "3:social" "social" "/home/chelovek/.config/i3/social.sh"
 
-# sleep 10
-# 
-# i3-msg "workspace 2:chrome"
-# sleep 1
-# # i3-msg "exec --no-startup-id google-chrome-stable notion.so && google-chrome-stable https://pizzabot.atlassian.net/jira/software/projects/PB/boards/1"
-# i3-msg "exec --no-startup-id google-chrome-stable"
-# sleep 7
-# 
-# i3-msg "workspace 3:social"
-# sleep 1
-# i3-msg "layout tabbed"
-# sleep 1
-# 
-# i3-msg "workspace 1:vim"
-# 
-# activeworkspace=$(i3-msg -t get_workspaces | jq -c '.[] | select(.focused) | .name' --raw-output)
-# atom &
-# windowname=atom
-# xprop -spy -root _NET_ACTIVE_WINDOW | \
-#   while read line ; do 
-#       if xprop WM_CLASS -id ${line##* } | grep -q $windowname ; then
-#           i3-msg move "[con_id=\"${line##* }\"] to workspace " $activeworkspace
-#           exit
-#       fi
-#   done
