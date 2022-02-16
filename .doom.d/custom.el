2962e652f0dbf011b37c68711e56e418d3d68598
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2022-02-16 14:37:16 +0500

    configure latex engine

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -3,8 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(LaTeX-default-author "Андрей Люнгрин")
  '(org-agenda-files
    '("~/projects/org/journal/2022-01-26.org" "/home/chelovek/.jira/PB.org" "/home/chelovek/projects/org/journal.org"))
+ '(org-latex-compiler "xelatex")
  '(org-roam-capture-templates
    '(("d" "default" plain "%?" :target
       (file+head "${slug}.org" "#+title: ${title}
