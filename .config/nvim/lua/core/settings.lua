6bedbee2e6093ee4b8639559e5d2d78bafd9c1c8
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2022-04-17 16:23:48 +0500

    set up all the basic stuff

---
+++ .config/nvim/lua/core/settings.lua
@@ -0,0 +1,27 @@
+local o = vim.opt
+local g = vim.g
+local map = vim.api.nvim_set_keymap
+
+map('n', '<Space>', '', {})
+g.mapleader = " "
+g.maplocalleader = " "
+
+g.do_filetype_lua = true
+
+o.number = true
+o.relativenumber = true
+o.signcolumn = "yes"
+
+o.tabstop = 4
+o.softtabstop = 4
+o.shiftwidth = 4
+o.expandtab = true
+o.smartindent = true
+
+o.smartcase = true
+o.incsearch = true
+
+o.hidden = true
+o.updatetime = 750
+o.termguicolors = true
+o.shortmess = o.shortmess + "c"
---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,37 @@
+-- Autocommand that reloads neovim whenever you save the packer_init.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+return require('packer').startup(function()
+    use 'wbthomason/packer.nvim'
+
+    use {
+        'lewis6991/gitsigns.nvim',
+        requires = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            require('gitsigns').setup()
+        end
+    }
+    use {
+        'folke/tokyonight.nvim',
+        config = function()
+            vim.g.tokyonight_style = "day"
+            vim.cmd[[colorscheme tokyonight]]
+        end
+    }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons' },
+        config = function()
+            require('lualine').setup {
+                options = {
+                    theme = 'tokyonight'
+                }
+            }
+        end
+    }
+end)
